108b85cabfb12515f985eb1e8d4ac3b6fda760fb
rifai <alliiyassir@gmail.com>
2024-03-04 18:52:43 +0100

    version 1.03

--- index.js
+++ index.js
@@ -2,21 +2,20 @@ const displayEl = document.getElementById("display");
 
 function append_to_display(input) {
   displayEl.value += input;
-
 }
 
 function calculate() {
   try {
     displayEl.value = eval(displayEl.value);
-
   } catch (error) {
     displayEl.value = "error";
   }
-
-
 }
 
 function clear_display() {
   displayEl.value = "";
+}
 
+function delete_last_element() {
+  displayEl.value = displayEl.value.slice(0, -1);
 }
